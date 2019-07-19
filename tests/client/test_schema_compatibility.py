918ab37dabde4399741361c65eebcded05730079
marcosschroh <schrohm@gmail.com>
2019-07-19 13:07:13 +0200

    test(Compatibility): Tests added to check global compatibiliies

--- tests/client/test_schema_compatibility.py
+++ tests/client/test_schema_compatibility.py
@@ -18,12 +18,20 @@ def test_compatibility(client, user_schema_v3):
     assert compatibility
 
 
-def test_update_compatibility(client):
+def test_update_compatibility_for_subject(client):
     """
     The latest User V2 schema is  BACKWARD and FORWARDFULL compatibility (FULL).
     So, we can ipdate compatibility level for the specified subject.
     """
     assert client.update_compatibility("FULL", "test-user-schema")
+
+
+def test_update_global_compatibility(client):
+    """
+    The latest User V2 schema is  BACKWARD and FORWARDFULL compatibility (FULL).
+    So, we can ipdate compatibility level for the specified subject.
+    """
+    assert client.update_compatibility("FULL")
 
 
 def test_update_compatibility_fail(client, response_klass, mocker):
@@ -38,8 +46,15 @@ def test_update_compatibility_fail(client, response_klass, mocker):
         assert excinfo.http_code == http_code
 
 
-def test_get_compatibility(client):
+def test_get_compatibility_for_subject(client):
     """
     Test latest compatibility for test-user-schema subject
     """
     assert client.get_compatibility("test-user-schema") == "FULL"
+
+
+def test_get_global_compatibility(client):
+    """
+    Test latest compatibility for test-user-schema subject
+    """
+    assert client.get_compatibility() is not None
