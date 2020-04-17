42bf42167277ea37b8a1fd07d6dafaa083703933
marcosschroh <schrohm@gmail.com>
2020-04-17 19:32:18 +0200

    fix: keep base url on client

--- schema_registry/client/paths.py
+++ schema_registry/client/paths.py
@@ -1,15 +1,15 @@
 # Available urls supported with the client
 
 paths = [
-    ("get_subjects", "/subjects", "GET"),
-    ("get_versions", "/subjects/{subject}/versions", "GET"),
-    ("delete_version", "/subjects/{subject}/versions/{version}", "DELETE"),
-    ("register", "/subjects/{subject}/versions", "POST"),
-    ("delete_subject", "/subjects/{subject}", "DELETE"),
-    ("get_schema", "/subjects/{subject}/versions/{version}", "GET"),
-    ("check_version", "/subjects/{subject}", "POST"),
-    ("get_by_id", "/schemas/ids/{schema_id}", "GET"),
-    ("test_compatibility", "/compatibility/subjects/{subject}/versions/{version}", "POST"),
-    ("update_compatibility", "/config/{subject}", "PUT"),
-    ("get_compatibility", "/config/{subject}", "GET"),
+    ("get_subjects", "subjects", "GET"),
+    ("get_versions", "subjects/{subject}/versions", "GET"),
+    ("delete_version", "subjects/{subject}/versions/{version}", "DELETE"),
+    ("register", "subjects/{subject}/versions", "POST"),
+    ("delete_subject", "subjects/{subject}", "DELETE"),
+    ("get_schema", "subjects/{subject}/versions/{version}", "GET"),
+    ("check_version", "subjects/{subject}", "POST"),
+    ("get_by_id", "schemas/ids/{schema_id}", "GET"),
+    ("test_compatibility", "compatibility/subjects/{subject}/versions/{version}", "POST"),
+    ("update_compatibility", "config/{subject}", "PUT"),
+    ("get_compatibility", "config/{subject}", "GET"),
 ]
--- tests/client/test_urls.py
+++ tests/client/test_urls.py
@@ -3,6 +3,10 @@ import urllib
 import pytest
 
 from schema_registry.client import urls
+from schema_registry.client.paths import paths
+
+
+BASE_URLS = ("http://localhost:8081", "http://localhost:8082/api/schema-registry/")
 
 
 def test_fail_url_manager_creation():
@@ -12,21 +16,41 @@ def test_fail_url_manager_creation():
         urls.UrlManager(base_url, [])
 
 
-def test_urls_generation():
-    base_url = "http://localhost:8081"
-
-    paths = [("get_cars", "/cars/{car_id}", "GET"), ("create_car", "/cars", "POST")]
+@pytest.mark.parametrize("base_url", BASE_URLS)
+def test_url_with_path(base_url):
+    paths = [("get_cars", "cars/{car_id}", "GET"), ("create_car", "cars", "POST")]
 
     url_manager = urls.UrlManager(base_url, paths)
-
     url, method = url_manager.url_for("get_cars")
-    assert url == urllib.parse.urljoin(base_url, "/cars/")
+
+    assert base_url in url
+
+
+@pytest.mark.parametrize("base_url", BASE_URLS)
+def test_urls_generation(base_url):
+    local_paths = [("get_cars", "cars/{car_id}", "GET"), ("create_car", "cars", "POST")]
+
+    url_manager = urls.UrlManager(base_url, local_paths)
+    url, method = url_manager.url_for("get_cars")
+
+    assert url == urllib.parse.urljoin(base_url, "cars/")
     assert method == "GET"
 
     url, method = url_manager.url_for("get_cars", car_id=10)
-    assert url == urllib.parse.urljoin(base_url, "/cars/10")
+    assert url == urllib.parse.urljoin(base_url, "cars/10")
     assert method == "GET"
 
     url, method = url_manager.url_for("create_car")
-    assert url == urllib.parse.urljoin(base_url, "/cars")
+    assert url == urllib.parse.urljoin(base_url, "cars")
     assert method == "POST"
+
+
+@pytest.mark.parametrize("base_url", BASE_URLS)
+def test_client_paths(base_url):
+    url_manager = urls.UrlManager(base_url, paths)
+
+    for func, path, _ in paths:
+        kwargs = {"subject": "my-subject", "version": 1}
+        url, method = url_manager.url_for(func, **kwargs)
+
+        assert base_url in url
