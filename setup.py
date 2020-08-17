6cd0cada8d2b6e287fe5cc8a1eefdc721a5c4213
jason <jason@zzq.org>
2020-08-17 10:43:40 -0600

    More permissive version pinning
    - Allow any 1.0 version of Faust. semver dictates these will all be compatible with one another.
    - Pin fastavro and httpx to their minor versions. These are not 1.0 releases and have unstable
      APIs. Pinning to the minor version should have a good balance between bug fixes and stability.
    - Don't pin test and docs. Incompatible changes won't break production systems.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ __version__ = "1.4.3"
 with open("README.md") as readme_file:
     long_description = readme_file.read()
 
-requires = ["fastavro==0.24.0", "httpx==0.14.1"]
+requires = ["fastavro>=0.24,<0.25", "httpx>=0.14,<0.15"]
 
 description = """Python Rest Client to interact against Schema Registry \
     Confluent Server to manage Avro Schemas
@@ -26,19 +26,19 @@ setup(
     author_email="schrohm@gmail.com",
     install_requires=requires,
     extras_require={
-        "faust": ["faust==1.10.4",],
-        "docs": ["mkdocs==1.1.2", "mkdocs-material==5.5.6",],
+        "faust": ["faust<2",],
+        "docs": ["mkdocs", "mkdocs-material",],
         "tests": [
-            "black==19.10b0",
-            "autoflake==1.3.1",
-            "flake8==3.8.3",
-            "mypy==0.782",
-            "isort==5.4.1",
-            "pytest==6.0.1",
-            "pytest-mock==3.2.0",
-            "faker==4.1.1",
-            "codecov==2.1.8",
-            "pytest-cov==2.10.0",
+            "black",
+            "autoflake",
+            "flake8",
+            "mypy",
+            "isort",
+            "pytest",
+            "pytest-mock",
+            "faker",
+            "codecov",
+            "pytest-cov",
         ],
     },
     url="https://github.com/marcosschroh/python-schema-registry-client",
