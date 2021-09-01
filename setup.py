e8faa17308055bb19a772cd4bb9c3e605ea9087c
marcosschroh <schrohm@gmail.com>
2021-09-01 13:23:31 +0200

    fix: update tests to last version of dataclasses-avroschema. fastavro and aiofiles updated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ __version__ = "1.8.2"
 with open("README.md") as readme_file:
     long_description = readme_file.read()
 
-requires = ["fastavro>=1.3.0", "httpx>=0.14,<0.15", "aiofiles>=0.5.0",]
+requires = ["fastavro>=1.4.4", "httpx>=0.14,<0.15", "aiofiles>=0.7.0",]
 
 description = "Python Rest Client to interact against Schema Registry Confluent Server to manage Avro Schemas"
 
