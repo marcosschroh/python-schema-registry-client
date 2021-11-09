2d940bf9a7659b29f9b2eb024b60090acc233769
Thiago Hiromi <thhi@hellofresh.com>
2021-11-09 23:39:45 +0100

    Add async message serializers in the root
    so the import from sync/async are consistent

--- schema_registry/serializers/__init__.py
+++ schema_registry/serializers/__init__.py
@@ -1,3 +1,6 @@
 from schema_registry.serializers.message_serializer import AvroMessageSerializer  # noqa
 from schema_registry.serializers.message_serializer import JsonMessageSerializer  # noqa
 from schema_registry.serializers.message_serializer import MessageSerializer  # noqa
+from schema_registry.serializers.message_serializer import AsyncAvroMessageSerializer  # noqa
+from schema_registry.serializers.message_serializer import AsyncJsonMessageSerializer  # noqa
+from schema_registry.serializers.message_serializer import AsyncMessageSerializer  # noqa
